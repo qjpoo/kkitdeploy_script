2f2e67ca5f4566ec42733ed1ce6d89e6ffdb53de
Administrator <b8_yang@163.com>
2019-08-12 13:03:50 +0800

    测试完毕

--- updateScript.sh
+++ updateScript.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 bash_path=$(cd "$(dirname "$0")";pwd)
+workPath = sys.argv[1]
 log="$bash_path/setup.log"  #操作日志存放路径
 fsize=2000000
 exec 2>>$log  #如果执行过程中有错误信息均输出到日志文件中
 updateScript(){
 unalias cp
-cd $bash_path
-git clone https://gitee.com/yb2018/kkitDeployScriptPackage.git && cd $bash_path/kkitDeployScriptPackage && cp -rf * ..
+
+git clone https://gitee.com/yb2018/kkitDeployScriptPackage.git $workPath  && cd $bash_path/kkitDeployScriptPackage && cp -rf * ..
 rm -rf $bash_path/kkitDeployScriptPackage
 }
 updateScript > ./setup.log 2>&1
