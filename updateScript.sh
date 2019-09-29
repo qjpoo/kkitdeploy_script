17438608b6bb03e3106eb224717c92b6b85720c4
boge <b8_yang@163.com>
2019-09-29 14:26:10 +0800

    修复升级脚本

--- updateScript.sh
+++ updateScript.sh
@@ -5,7 +5,9 @@ log="$bash_path/setup.log"  #操作日志存放路径
 fsize=2000000
 exec 2>>$log  #如果执行过程中有错误信息均输出到日志文件中
 updateScript(){
-unalias cp
+#unalias cp
+`sed -i "/^alias cp='cp -i'/d" /root/.bashrc`
+
 #test -d ./kkitDeployScriptPackage || mkdir -p ./kkitDeployScriptPackage 
 git clone https://gitee.com/yb2018/kkitDeployScriptPackage.git "${workPath}"kkitDeployScriptPackage && cd "${workPath}"kkitDeployScriptPackage && cp -rf * $workPath
 cd $workPath && rm -rf ./kkitDeployScriptPackage
